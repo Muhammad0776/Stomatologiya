ab2c49f3caedea84acd7e1ab19c0f26c1451deb3
Mukhammad <81352000+Muhammad0776@users.noreply.github.com>
2021-09-05 07:50:40 +0500

    containers

--- containers/Header/Header.js
+++ containers/Header/Header.js
@@ -1,10 +1,110 @@
-import React from "react";
+import {
+  faGoogle,
+  faInstagram,
+  faInvision,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faCalendar,
+  faMapMarker,
+  faMapMarkerAlt,
+  faPhone,
+  faPhoneAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from "reactstrap";
 import HeaderWrapper from "./HeaderWrapper";
 
-const Header = () => {
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <HeaderWrapper>
-      <h1>Header</h1>
+      <div className="container">
+        {/* Navbar start */}
+        <Navbar light expand="md">
+          <NavbarBrand href="/">
+            <img className="logo" src="/images/logo.png" alt="" />
+          </NavbarBrand>
+          <div className="d-flex ms-4 me-4">
+            <a className="icons me-3" href="#">
+              <FontAwesomeIcon icon={faGoogle} />
+            </a>
+            <a className="icons me-3" href="#">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a className="icons me-3" href="#">
+              <FontAwesomeIcon icon={faInvision} />
+            </a>
+          </div>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto pe-4" navbar>
+              <NavItem>
+                <NavLink className="nav-title" href="/">
+                  О нас
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-title" href="/">
+                  Услуги
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-title" href="/">
+                  Специалисты
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-title" href="/">
+                  Пациентам
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-title" href="/">
+                  Контакты
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <div className="d-flex pe-4">
+              <FontAwesomeIcon className="me-2 mt-3" icon={faMapMarkerAlt} />
+              <p className=" mt-3">
+                Чебоксары, <br /> Энтузиастов 1А{" "}
+              </p>
+            </div>
+            <div className=" d-flex pe-4">
+              <FontAwesomeIcon className="me-2 mt-3" icon={faPhoneAlt} />
+              <p className="fw-bold mt-3">+7900 333 10 40</p>
+            </div>
+            <Button className="" variant="contained" color="primary">
+              Заказать звонок
+            </Button>
+          </Collapse>
+        </Navbar>
+
+        {/* Header start */}
+        <Header className="bg-imag">
+          <div className="sidebar text-center">
+            <p>— Стоматология в Чебоксарах</p>
+          </div>
+        </Header>
+      </div>
     </HeaderWrapper>
   );
 };
--- containers/Header/HeaderWrapper.js
+++ containers/Header/HeaderWrapper.js
@@ -1,5 +1,56 @@
 import styled from "styled-components";
+// import img from "../../public/images/bg-head.png";
 
-const HeaderWrapper = styled.div``;
+const HeaderWrapper = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap");
+
+  .logo {
+    width: 57px;
+    height: 51px;
+  }
+
+  .icons {
+    width: 15px;
+    height: 15px;
+  }
+
+  .nav-title {
+    font-family: Roboto, sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 18px;
+    color: #aaaaaa;
+  }
+  .nav-title:hover {
+    color: #217a99 !important;
+  }
+
+  p {
+    font-family: Arial;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 13.5px !important;
+    line-height: 17px;
+    color: #474747;
+    cursor: alias;
+  }
+
+  Button {
+    font-size: 12px;
+    background: #38bcea;
+    border-radius: 3px;
+  }
+  Button:hover {
+    background: #38bcea;
+  }
+
+  ${"" /* Header style */}
+  .bg-imag {
+    ${"" /* background-image: url({img}); */}
+    background-size: cover;
+    background-position: 50% 50%;
+  }
+`;
 
 export default HeaderWrapper;
